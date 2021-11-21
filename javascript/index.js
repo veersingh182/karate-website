fce52fbd23d3800fea2b33fa5ba809ab0c16339d
Virendra Saini <veerugsaini@gmail.com>
2021-11-21 20:37:48 +0530

    responsive home page

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,19 @@
-window.addEventListener('scroll',()=>{
+let navbar = document.querySelector('.navbar');
 
-    let navbar = document.querySelector('.navbar');
+window.addEventListener('scroll',()=>{
     let windowPosition = window.scrollY > 0;
 
     navbar.classList.toggle('navbar-bg', windowPosition);
 
-})
+});
+
+let nav_small = document.getElementById('nav-small');
+let menu = document.getElementsByClassName('menu');
+
+nav_small.addEventListener('click',()=>{
+
+    navbar.classList.toggle('bgColor_nav_small');
+    navbar.classList.toggle('navbar-bg', windowPosition);
+    menu.classList.toggle('menu_display_block');
+
+});
--- slick/js/main.js
+++ slick/js/main.js
@@ -2,29 +2,21 @@ $(document).ready(function(){
     var resWidth = screen.width;
     
     if (resWidth < 800) {
-      // refresh variables
-      // Apply variables again
       $('.responsive').slick({
         dots: true,
         infinite: true,
-        // speed: 300,
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        // autoplaySpeed: 3500,
       });
     }
     else if (resWidth < 1200) {
-      // refresh variables
-      // Apply variables again
       $('.responsive').slick({
         dots: true,
         infinite: true,
-        // speed: 300,
         slidesToShow: 2,
         slidesToScroll: 2,
         autoplay: true,
-        // autoplaySpeed: 3500,
       });
     }
     
@@ -32,22 +24,19 @@ $(document).ready(function(){
       $('.responsive').slick({
         dots: true,
         infinite: true,
-        // speed: 300,
         slidesToShow: 3,
         slidesToScroll: 3,
         autoplay: true,
-        // autoplaySpeed: 3500,
         });
       }
 });
 
-
 $(document).ready(function(){
-  $('.one-time').slick({
+  $('.one-review-only').slick({
     dots: true,
     infinite: true,
-    speed: 300,
     slidesToShow: 1,
-    autoplay:true,
+    slidesToScroll: 1,
+    autoplay: true,
   });
 });
