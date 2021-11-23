343511f1b278520249a38597cb0152a227439807
Virendra Saini <veerugsaini@gmail.com>
2021-11-23 16:23:12 +0530

    completed About Us and Its four  sub sections (all Responsive)

--- javascript/MissionVissionHistory.js
+++ javascript/MissionVissionHistory.js
@@ -1,12 +1,4 @@
 let navbar = document.querySelector('.navbar');
-
-window.addEventListener('scroll',()=>{
-    let windowPosition = window.scrollY > 0;
-
-    navbar.classList.toggle('navbar-bg', windowPosition);
-
-});
-
 let nav_small = document.getElementById('nav-small');
 let menu = document.getElementById('menu');
 nav_small.addEventListener('click',()=>{
