841f1e98819d29ad6e05bdcdf8b8a425507f8518
Virendra Saini <veerugsaini@gmail.com>
2021-12-05 20:22:32 +0530

    About Us with All 4 subPages and Eventpage and Gallery

--- javascript/index.js
+++ javascript/index.js
@@ -2,13 +2,13 @@ let navbar = document.querySelector('.navbar');
 
 window.addEventListener('scroll',()=>{
     let windowPosition = window.scrollY > 0;
-
     navbar.classList.toggle('navbar-bg', windowPosition);
 
 });
 
 let nav_small = document.getElementById('nav-small');
 let menu = document.getElementById('menu');
+
 nav_small.addEventListener('click',()=>{
     
     navbar.classList.toggle('bgColor_nav_small');
--- slick/js/main.js
+++ slick/js/main.js
@@ -1,35 +1,32 @@
-$(document).ready(function(){
-    var resWidth = screen.width;
-    
-    if (resWidth < 800) {
-      $('.responsive').slick({
-        dots: true,
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-      });
-    }
-    else if (resWidth < 1200) {
-      $('.responsive').slick({
-        dots: true,
-        infinite: true,
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        autoplay: true,
-      });
-    }
-    
-    else{
-      $('.responsive').slick({
-        dots: true,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        autoplay: true,
-        });
-      }
-});
+let resWidth = window.innerWidth;
+if (resWidth < 800) {
+  $('.responsive').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  });
+}
+else if (resWidth < 1200) {
+  $('.responsive').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    autoplay: true,
+  });
+}
+
+else{
+  $('.responsive').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+  });
+}
 
 $(document).ready(function(){
   $('.one-review-only').slick({
